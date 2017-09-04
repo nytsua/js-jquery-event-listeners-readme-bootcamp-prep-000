aa665dbfa720597c02fc71ea7cf51b7d4dc5c373
Austyn Trace <atrace@gmail.com>
2017-09-04 16:35:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt(){
 function pressIt(){
   $('#typing').on('keydown', function(key){
     if(key.which == 71){
-      alert('s was pressed');
+      alert('you pressed g');
     }
   });
 }
